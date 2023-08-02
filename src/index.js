9153fbde66f53b1e24d61bc65bc53f110a23fb0a
Anton Antipov <managerantonantipov@yandex.ru>
2023-08-02 16:45:14 +0700

    добавил картинку при условии

--- src/index.js
+++ src/index.js
@@ -17,13 +17,21 @@ const cssButton = {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let snabj = ['Колтышева Таня', 'Фокина Аня', 'Пименова Виктория', 'Зенина Настя', 'Артамонова Марина', 'Артамонова Даша', 'Емельянова Ольга',
   'Цветкова Ольга', 'Антипов Антон'];
-
+ 
 function randomPeopleSnab() {
   const element = document.querySelector('#result');
+  
   let randomIndex = Math.floor(Math.random() * snabj.length);
   let selected = snabj[randomIndex];
   element.textContent = selected;
+  
+ if(element.textContent === 'Цветкова Ольга')
+ 
+ document.querySelector('#img').src = 'https://sun9-6.userapi.com/impg/4MzeO1MlKSxhNADYJt2lzvN775_k4jFt4TAYhw/B3zOOE4GfoU.jpg?size=1802x1920&quality=95&sign=58973b96cfa6c7c0a01c934f76a86818&c_uniq_tag=ChjGqdfauYXmMh1n-kPrZDtu7MSrse-i1sbWjQppD8w&type=album'
+  
+ 
 }
+
 root.render(
   <React.StrictMode>
     <div style={{
@@ -39,6 +47,10 @@ root.render(
         Нажмите, чтобы узнать
       </button>
       <h2 id="result"></h2>
+      <img id='img' style={{
+        height:200,
+        
+      }} src=''/>
     </div>
   </React.StrictMode>
 );

9073d29cd11faf96fe62a08a6735549725c53457
Anton Antipov <managerantonantipov@yandex.ru>
2023-08-02 10:45:01 +0700

    первое тестовое приложение

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const cssButton = {
+  marginBottom: 37,
+  marginTop: 20,
+  width: 358,
+  height: 50,
+  fontSize: '21px',
+  background: '#000000',
+  borderRadius: 2,
+  color: '#ffffff',
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let snabj = ['Колтышева Таня', 'Фокина Аня', 'Пименова Виктория', 'Зенина Настя', 'Артамонова Марина', 'Артамонова Даша', 'Емельянова Ольга',
+  'Цветкова Ольга', 'Антипов Антон'];
+
+function randomPeopleSnab() {
+  const element = document.querySelector('#result');
+  let randomIndex = Math.floor(Math.random() * snabj.length);
+  let selected = snabj[randomIndex];
+  element.textContent = selected;
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <h1 style={{
+        textAlign: 'center',
+      }}>Кто лучший сотрудник месяца?</h1>
+      <button style={cssButton} onClick={randomPeopleSnab}>
+        Нажмите, чтобы узнать
+      </button>
+      <h2 id="result"></h2>
+    </div>
   </React.StrictMode>
 );
 
